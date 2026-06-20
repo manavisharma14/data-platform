e55f3adfa42a06fb5e3d3a328c2cb3c0789bb36b
manavisharma14 <manavisharma14@gmail.com>
2026-06-19 21:45:57 -0500

    created dag object

--- airflow/dags/event_pipeline.py
+++ airflow/dags/event_pipeline.py
@@ -1,3 +1,11 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
+
+with DAG(
+    dag_id="event_pipeline",
+    start_date=datetime(2025,1,1),
+    schedule=None,
+    catchup=False,
+    tags=["genpact"]
+) as dag:
