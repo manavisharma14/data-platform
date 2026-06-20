43350c68a866c49b92b797a848f5a4513fd5d93c
manavisharma14 <manavisharma14@gmail.com>
2026-06-20 12:13:20 -0500

    updated events load

--- ingestion/load_events_copy.py
+++ ingestion/load_events_copy.py
@@ -1,2 +1,40 @@
 import psycopg2
 
+conn = psycopg2.connect(
+    host="genpact-postgres",
+    database="genpact_etl",
+    user="postgres",
+    password="postgres"
+)
+
+cur = conn.cursor()
+
+csv_file = "/project/data/rees46/2019-Nov.csv"
+
+with open(csv_file, "r") as f:
+    cur.copy_expert(
+        """
+        COPY staging_events (
+            event_time,
+            event_type,
+            product_id,
+            category_id,
+            category_code,
+            brand,
+            price,
+            user_id,
+            user_session
+        )
+        FROM STDIN
+        WITH (
+            FORMAT CSV,
+            HEADER TRUE
+        )
+        """,
+        f
+    )
+conn.commit()
+cur.close()
+conn.close()
+
+print("COPY COMPLETED")
