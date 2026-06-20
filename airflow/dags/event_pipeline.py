25e1b553bbba1c2bdf600ce6aa4aa8061c4eca7b
manavisharma14 <manavisharma14@gmail.com>
2026-06-19 21:48:26 -0500

    task objects created

--- airflow/dags/event_pipeline.py
+++ airflow/dags/event_pipeline.py
@@ -9,3 +9,34 @@ with DAG(
     catchup=False,
     tags=["genpact"]
 ) as dag:
+    
+    load_events = BashOperator(
+        task_id="load_events",
+        bash_command="python /project/ingestion/load_events.py"
+    )
+
+    validate_events = BashOperator(
+
+        task_id="validate_events",
+
+        bash_command="python /project/validation/validate_events.py"
+
+    )
+
+    retry_failed_events = BashOperator(
+
+        task_id="retry_failed_events",
+
+        bash_command="python /project/validation/retry_failed_events.py"
+
+    )
+
+    build_warehouse = BashOperator(
+
+        task_id="build_warehouse",
+
+        bash_command="python /project/warehouse/build_warehouse.py"
+
+    )
+
+    load_events >> validate_events >> retry_failed_events >> build_warehouse
